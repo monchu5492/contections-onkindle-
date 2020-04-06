6941b4ff366d518b89bc5c5122705cda24e61852
monchu5492 <39810970+monchu5492@users.noreply.github.com>
2020-04-06 00:19:42 -0700

    made note from amazon fire tablet for fun

--- src/App.js
+++ src/App.js
@@ -61,6 +61,8 @@ export default class App extends React.Component {
       });
   }
 
+//this is just anote from the amazon fire 
+
   getAllUsers() {
     console.log("getting all users");
     fetch(userURl, {
